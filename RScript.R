3a8b21b4d0381778e600a37924cdfa7ef6336c65
Mukul Parashar <mukulparashar0413@gmail.com>
2019-10-23 13:38:43 +0530

    Update RScript.R

--- RScript.R
+++ RScript.R
@@ -2,15 +2,20 @@
 install.packages("tidyverse")
 library(tidyverse)
 
-usedcars <- read_csv(usedcars.csv)
+usedcars <- read_csv("usedcar.csv")
 
 summary(usedcars)
 colSums(is.na(usedcars))
 
+
+usedcars <- usedcars %>% mutate(Manufacturer = substr(Name,1,regexpr(" ",Name)-1))
+usedcars$Manufacturer <- str_replace(df$Manufacturer,"Land","Land-Rover")
+usedcars$Mileage <- as.numeric(substr(usedcars$Mileage,1,regexpr(" ",usedcars$Mileage)-1))
+
 na.omit(usedcars)
 
-df <- usedcars %>% mutate(Manufacturer = substr(Name,1,regexpr(" ",Name)-1))
-df$Manufacturer <- str_replace(df$Manufacturer,"Land","Land-Rover")
+df <- usedcars
+
 
 sell <- df %>% group_by(City = Location) %>% summarise(No_of_Cars_on_Sell = n())
 
@@ -21,7 +26,24 @@ ggplot(data = sell) +
   scale_x_discrete("City") +
   theme(axis.text.x = element_text(angle = 30, vjust = 1)) +
   scale_fill_brewer(palette="Paired")
-  
+
+
+
+
+carloc <- select(df, c(Location, Owner_Type))
+carloc <- carloc %>% group_by(Location, Owner_Type) %>% summarise(No_of_Cars = n())
+
+ggplot(carloc[order(carloc$Owner_Type, decreasing = T),]) +
+  geom_bar(mapping = aes(x = Location, y = No_of_Cars, fill = factor(Owner_Type, levels=c("Fourth & Above","Third","Second","First" ))), 
+           position = "fill",
+           stat = "identity") + 
+  scale_y_continuous("Percentage of Cars", labels = scales::percent) +
+  scale_x_discrete("City") +
+  scale_fill_brewer(palette="Greens") +
+  theme_linedraw() +
+  labs(fill = "Owner_Type") +
+  theme(axis.text.x = element_text(angle = 30, vjust = 0.5))
+
   
   
   
@@ -43,10 +65,6 @@ df2 <- select(df,Fuel_Type,Kilometers_Driven, Manufacturer, Owner_Type, Mileage,
 a <- df2 %>% group_by(Manufacturer, Fuel_Type) %>% summarise(No_of_Cars_on_Sell = n())
 a <- a %>% filter(Manufacturer %in% c("Maruti","Hyundai","Honda","Toyota","Mercedes-Benz","Volkswagen","Ford","Mahindra","BMW","Audi"))
 
-b <- df2 %>% group_by(Manufacturer, Owner_Type) %>% summarise(No_of_Cars_on_Sell = n())
-b <- b %>% filter(Manufacturer %in% c("Maruti","Hyundai","Honda","Toyota","Mercedes-Benz","Volkswagen","Ford","Mahindra","BMW","Audi"))
-
-
   
   
 ggplot(data = df2) +
@@ -58,8 +76,21 @@ ggplot(data = a) +
   scale_fill_brewer(palette="Blues") +
   theme(axis.text.x = element_text(angle = 30, vjust = 1))
 
+ggplot(data = df)+
+  geom_histogram(mapping = aes(x = Fuel_Type, y = Mileage, fill = Owner_Type), stat = "identity", position = "dodge")
+
 ggplot(data = df) +
   geom_histogram(mapping = aes(x = Year, fill =  Transmission), position = "dodge", bins = 100) +
   scale_y_continuous("No of Cars on Sell") +
+  scale_x_continuous("Year of Manufacture")
+
+ggplot(data = df) +
+  geom_bar(mapping = aes(x = Year, y = Mileage,  fill = Transmission), stat = "identity", position = "dodge", bins = 50) +
+  scale_y_continuous("Mileage") +
+  scale_x_continuous("Year of Manufacture")
+
+ggplot(data = df) +
+  geom_bar(mapping = aes(x = Year, y = Mileage,  fill = Fuel_Type), stat = "identity", position = "dodge", bins = 50) +
+  scale_y_continuous("Mileage") +
   scale_x_continuous("Year of Manufacture")
  
